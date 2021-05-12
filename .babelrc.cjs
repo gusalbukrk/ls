8436aa10552844279fc02e21d3385fa7717950be
gusalbukrk <gusalbukrk@gmail.com>
2021-05-12 11:38:39 -0300

    add babel

---
+++ .babelrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        // 'usage' = add the polyfills needed automatically
+        // 'entry' = requires explicit import of core-js
+        useBuiltIns: 'usage',
+        corejs: {
+          version: '3.8', // change it to the last version
+          proposals: true,
+        },
+      },
+    ],
+  ],
+};
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,16 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import { babel } from '@rollup/plugin-babel';
 
 export default {
   input: 'src/index.js',
   plugins: [
     nodeResolve(),
     commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+    }),
   ],
   output: [
     {
