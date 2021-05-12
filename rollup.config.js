8d3dbd3ae0318b2011439ff97a382a88e16f0a21
gusalbukrk <gusalbukrk@gmail.com>
2021-05-12 11:49:30 -0300

    add typescript

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,22 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import { babel } from '@rollup/plugin-babel';
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+import typescript from 'rollup-plugin-typescript2';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   plugins: [
     nodeResolve(),
     commonjs(),
+    typescript(),
     babel({
       babelHelpers: 'bundled',
+      extensions: [
+        ...DEFAULT_EXTENSIONS,
+        '.ts',
+        '.tsx',
+      ],
     }),
   ],
   output: [
