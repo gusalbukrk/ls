0214bd08f97e15db2012a70e030a72089d015c87
gusalbukrk <gusalbukrk@gmail.com>
2021-06-01 08:44:44 -0300

    edit lint-staged config

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,11 @@
 // lint-staged will automatically add any fix to the commit
 // that's why the linting of test files is done here
+// husky will execute jest soon after, but without eslint (see '.husky/pre-commit', line 5)
 
 module.exports = {
-  '*.(js|jsx|cjs|ts|tsx)': `eslint --fix`, // including test files
-  '*.md': 'prettier --write',
+  '*.(js|cjs|json)': 'npm run lint:config',
+  '*.(js|jsx|ts|tsx|json)': `eslint --fix`,
+
+  // json needs to be included here because eslint doesn't remove multiple blank lines in json files, prettier do
+  '*.(md|json)': 'prettier --write',
 };
